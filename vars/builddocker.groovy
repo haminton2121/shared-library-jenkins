f223d686f187890e8a427b84a9358a796be237b7
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-20 13:39:26 +0700

    Update builddocker.groovy

--- vars/builddocker.groovy
+++ vars/builddocker.groovy
@@ -1,8 +1,8 @@
 def builddocker() {
     // Generate the DPLVERSION based on the current date and build number
-    def now = new Date()
-    def setDate = now.format("yyyy.MM.dd", TimeZone.getTimeZone('UTC'))
-    def DPLVERSION = "${setDate}.${BUILD_NUMBER}"
+//    def now = new Date()
+//    def setDate = now.format("yyyy.MM.dd", TimeZone.getTimeZone('UTC'))
+//    def DPLVERSION = "${setDate}.${BUILD_NUMBER}"
     
     // Ensure proper interpolation within the shell script
     sh """
@@ -15,15 +15,15 @@ def builddocker() {
         cat Dockerfile
 
         # Build the Docker image and tag it with DPLVERSION
-        docker build -t 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com/hnguyen/test/nginx:${DPLVERSION} .
+        docker build -t 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com/hnguyen/test/nginx:latest .
         
         # Log in to AWS ECR
         /usr/local/bin/aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com
         
         # Push the image to AWS ECR
-        docker push 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com/hnguyen/test/nginx:${DPLVERSION}
+        docker push 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com/hnguyen/test/nginx:latest
         
         # Optionally, remove the image after pushing
-        docker rmi 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com/hnguyen/test/nginx:${DPLVERSION}
+        docker rmi 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com/hnguyen/test/nginx:latest
     """
 }
