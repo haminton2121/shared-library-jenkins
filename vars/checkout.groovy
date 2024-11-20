e60057a735c0bd3165111cece38dc809adec109c
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-20 09:45:12 +0700

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,28 @@
-def call(Map stageParams) {
-                checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]], doGenerateSubmoduleConfigurations: false,
-                          extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: stageParams.dir]],
-                submoduleCfg: [], userRemoteConfigs: [[credentialsId: stageParams.credential_git , url: stageParams.url]]])
+//def call(Map stageParams) {
+//                checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]], doGenerateSubmoduleConfigurations: false,
+//                          extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: stageParams.dir]],
+//                submoduleCfg: [], userRemoteConfigs: [[credentialsId: stageParams.credential_git , url: stageParams.url]]])
+//}
+def call(Map config) {
+    def targetBranch = config.targetBranch ?: 'main' // Default to 'main' if no branch provided
+    def service = config.service ?: 'defaultService' // Default service name if not provided
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${targetBranch}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [
+            [
+                $class: 'RelativeTargetDirectory',
+                relativeTargetDir: "AsiaPolicyUI${service}"
+            ]
+        ],
+        submoduleCfg: [],
+        userRemoteConfigs: [
+            [
+                credentialsId: config.credentialsId ?: 'hnguyen421_git',
+                url: "https://github.dxc.com/insurance/AsiaPolicyUI${service}"
+            ]
+        ]
+    ])
 }
