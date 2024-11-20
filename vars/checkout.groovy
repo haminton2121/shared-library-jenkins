eed82e009aead265df8ecce37fe6b75565e48797
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-20 08:59:14 +0700

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,5 @@
 def call(Map stageParams) {
-                checkout([$class: 'GitSCM', branches: [[name: */stageParams.branch]], doGenerateSubmoduleConfigurations: false,
+                checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]], doGenerateSubmoduleConfigurations: false,
                           extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: stageParams.dir]],
                 submoduleCfg: [], userRemoteConfigs: [[credentialsId: stageParams.credential_git , url: stageParams.url]]])
 }
