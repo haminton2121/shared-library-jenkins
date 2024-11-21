1cf510f0a889acdb36d70393cb9cb9f522c5c0fe
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-21 10:59:08 +0700

    Update builddocker.groovy

--- vars/builddocker.groovy
+++ vars/builddocker.groovy
@@ -1,7 +1,5 @@
 def call(Map config) {
-    // Khai báo AWS credentials trước khi gọi bất kỳ lệnh AWS nào
     awscredential.awscredential {
-        // Gọi hàm uploadToS3 từ awsUtils.groovy để upload file lên S3
         builddockerimage.call(Map config)
     }
 }
