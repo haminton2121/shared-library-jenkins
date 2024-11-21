c13217c2eb9aa1c4065eca39a85b3f80898fca05
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-21 14:01:01 +0700

    Update awscre.groovy

--- vars/awscre.groovy
+++ vars/awscre.groovy
@@ -1,8 +1,16 @@
+
+// vars/awsUtils.groovy
 def call() {
-//    def AWS_CREDENTIALS = config.credentialsId 
-//    def AWS_REGION = config.region ?: 'ap-southeast-1' 
-    withAWS(credentials: 'my-aws-credentials', region: 'ap-southeast-1') {
-        // Now you can call AWS commands using these credentials
+
+    // Use withCredentials to bind AWS credentials from Jenkins to environment variables
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'my-aws-credential']]) {
+        // AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are now available as environment variables
         echo "AWS credentials are successfully set up."
+        
+        // Log in to AWS ECR using the injected credentials
+        sh """
+            echo "Logging into AWS ECR..."
+            aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com
+        """
     }
 }
