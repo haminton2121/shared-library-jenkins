bf0a07e1ecfb448477fa0275ea1d043a15268e32
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-21 10:41:18 +0700

    Update builddocker.groovy

--- vars/builddocker.groovy
+++ vars/builddocker.groovy
@@ -1,28 +1,7 @@
 def call(Map config) {
-    def now = new Date()
-    def setDate = now.format("yyyy.MM.dd", TimeZone.getTimeZone('UTC'))
-    def DPLVERSION = "${setDate}.${BUILD_NUMBER}"
-    def registryUrl = config.registryUrl
-    def pathImage = config.pathImage
-    def dockerfile = libraryResource "docker/Dockerfile"
-    writeFile file: "${WORKSPACE}/Dockerfile", text: dockerfile
-    def index = libraryResource "docker/index.html"
-    writeFile file: "${WORKSPACE}/index.html", text: index
-    sh """
-        echo "Building Docker Image with version"  
-        # Display the Dockerfile contents (optional)
-        cat Dockerfile
-
-        # Build the Docker image and tag it with ${DPLVERSION}
-        docker build -t ${registryUrl}/${pathImage}:${DPLVERSION} .
-        
-        # Log in to AWS ECR
-        /usr/local/bin/aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${registryUrl}
-        
-        # Push the image to AWS ECR
-        docker push ${registryUrl}/${pathImage}:${DPLVERSION}
-        
-        # Optionally, remove the image after pushing
-        docker rmi ${registryUrl}/${pathImage}:${DPLVERSION}
-    """
+    // Khai báo AWS credentials trước khi gọi bất kỳ lệnh AWS nào
+    awscredential.awscredential {
+        // Gọi hàm uploadToS3 từ awsUtils.groovy để upload file lên S3
+        builddockerimage.call(Map config)
+    }
 }
