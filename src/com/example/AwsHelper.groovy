6fbef0ce7653ce0d553cae189dcbcbba5f553a7d
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-22 09:33:39 +0700

    Update AwsHelper.groovy

--- src/com/example/AwsHelper.groovy
+++ src/com/example/AwsHelper.groovy
@@ -3,7 +3,7 @@ package com.example
 class AwsHelper {
     def loginToECR() {
 
-        sh """
+        return """
             echo "Logging into AWS ECR..."
             aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com
         """
