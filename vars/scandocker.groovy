3bbcff570e31767b882778b7049d9374f7eecc12
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-25 10:48:53 +0700

    Update scandocker.groovy

--- vars/scandocker.groovy
+++ vars/scandocker.groovy
@@ -1,4 +1,5 @@
 def call(String registryUrl, String pathImage){
+    def now = new Date()
     def setDate = now.format("yyyy.MM.dd", TimeZone.getTimeZone('UTC'))
     def DPLVERSION = "${setDate}.${BUILD_NUMBER}"
     sh """
