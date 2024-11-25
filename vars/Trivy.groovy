023505f77cb0327b0d230a414c48a51a4d87ebc9
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-25 10:33:02 +0700

    Update Trivy.groovy

--- vars/Trivy.groovy
+++ vars/Trivy.groovy
@@ -2,7 +2,7 @@ def call(String registryUrl, String pathImage){
     def setDate = now.format("yyyy.MM.dd", TimeZone.getTimeZone('UTC'))
     def DPLVERSION = "${setDate}.${BUILD_NUMBER}"
     sh """
-    trivy image ${registryUrl}/${pathImage}:${DPLVERSION} > a.txt
+    /usr/local/bin/trivy image ${registryUrl}/${pathImage}:${DPLVERSION} > a.txt
     cat a.txt
     """
 }
