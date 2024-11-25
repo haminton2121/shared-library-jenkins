e2d5665dce5e42fddd2136ec94a4008ed02b823f
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-25 09:50:32 +0700

    Update loginECR.groovy

--- vars/loginECR.groovy
+++ vars/loginECR.groovy
@@ -1,8 +1,8 @@
 // vars/awsUtils.groovy
-def call(String credentials, String region, String imagePath) {
+def call(String region, String imagePath) {
 
     // Use withCredentials to bind AWS credentials from Jenkins to environment variables
-    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: ${credentials}]]) {
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'my-aws-credentials']]) {
         // AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are now available as environment variables
         echo "AWS credentials are successfully set up."
         
