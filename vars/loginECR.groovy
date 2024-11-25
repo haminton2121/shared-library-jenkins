d018e427eeef5a71ae146a1fa7d03265c33f0cf5
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-25 09:30:30 +0700

    Create loginECR.groovy

---
+++ vars/loginECR.groovy
@@ -0,0 +1,15 @@
+// vars/awsUtils.groovy
+def call(String region, String imagePath) {
+
+    // Use withCredentials to bind AWS credentials from Jenkins to environment variables
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: ${credentials}]]) {
+        // AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are now available as environment variables
+        echo "AWS credentials are successfully set up."
+        
+        // Log in to AWS ECR using the injected credentials
+        sh """
+            echo "Logging into AWS ECR..."
+            aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${imagePath}
+        """
+    }
+}
