06a52e5be5ac09c70038f77f0a800af868c0f9a7
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-26 16:00:52 +0700

    Update tfgetcode.groovy

--- vars/tfgetcode.groovy
+++ vars/tfgetcode.groovy
@@ -3,7 +3,7 @@ def call(Map config) {
     def branch           = config.branch  // Will be defined as an environment name.
     def deploymentUnits  = config.deploymentUnits  // Fixed typo: "test" should map the deploymentUnits correctly
     def duRepoList = '''DocumentDB:documentdb'''.replaceAll("\n", " ")
-    def githubRepo = sh(returnStdout: true, script: "echo ${duRepoList} | tr ' ' '\n' | grep ^${deploymentUnit}: | cut -d':' -f2 || echo ''").trim()
+    def githubRepo = sh(returnStdout: true, script: "echo ${duRepoList} | tr ' ' '\n' | grep ^${deploymentUnits}: | cut -d':' -f2 || echo ''").trim()
     // Using deploymentUnits instead of deploymentUnit (fixed typo)
 
     // Use withCredentials block for GitHub credentials
