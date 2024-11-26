76b6208957812d91a1ae93dabe23c6d78628000f
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-26 14:34:20 +0700

    Update tfgetcode.groovy

--- vars/tfgetcode.groovy
+++ vars/tfgetcode.groovy
@@ -11,6 +11,7 @@ def call(Map config) {
     withCredentials([usernamePassword(credentialsId: 'hoanguyengit', usernameVariable: 'gitUsername', passwordVariable: 'gitPassword')]) {
         sh '''
             rm -rf ${githubRepo} || true
+            apk update
             apk add git
             #git config --global credential.helper '!f() { sleep 1; echo "username=${gitUsername}"; echo "password=${gitPassword}"; }; f'
             sudo git config --system user.name "My Name"
