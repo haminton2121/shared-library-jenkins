609c8386f093e14605a625beb4ea1ef6ed2cbd9e
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-27 09:52:34 +0700

    Update tfgetcode.groovy

--- vars/tfgetcode.groovy
+++ vars/tfgetcode.groovy
@@ -2,11 +2,13 @@ def call(Map config) {
     def orgGithub        = 'terraform194920'
     def branch           = config.branch  // Will be defined as an environment name.
     def deploymentUnits  = config.deploymentUnits  // Fixed typo: "test" should map the deploymentUnits correctly
+    def deploymentAction = config.deploymentAction
     def duRepoList = '''DocumentDB:documentdb'''.replaceAll("\n", " ")
     def githubRepo = sh(returnStdout: true, script: "echo ${duRepoList} | tr ' ' '\n' | grep ^${deploymentUnits}: | cut -d':' -f2 || echo ''").trim()
     // Using deploymentUnits instead of deploymentUnit (fixed typo)
 
     // Use withCredentials block for GitHub credentials
+    if (deploymentUnits != 'None') {
     withCredentials([usernamePassword(credentialsId: 'hoanguyengit', usernameVariable: 'gitUsername', passwordVariable: 'gitPassword')]) {
         sh """
             echo "Cloning repository: ${githubRepo}"
@@ -15,8 +17,17 @@ def call(Map config) {
             git clone -b ${branch} --single-branch "https://gitlab.com/${orgGithub}/${githubRepo}.git"
 
             git config --global --remove-section credential
+            cd ${WORKSPACE}/${githubRepo}
+                    terraform init
+                    terraform validate
+                    terraform fmt
+                    terraform plan -var-file="terraform.tfvars" -out=tfplan
+            if [ "${deploymentAction}" == "apply" ]; then
+            terraform apply -auto-approve tfplan
+            elif [ "${deploymentAction}" == "destroy" ]; then
+            terraform destroy -auto-approve
+            fi
         """
     }
-    initTF(githubRepo)
-    TFaction(githubRepo)
+}
 }
