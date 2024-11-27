53fce139baf248640d1c46c6db0a4174017e8a1c
haminton2121 <63803013+haminton2121@users.noreply.github.com>
2024-11-27 10:09:21 +0700

    Update awscre.groovy

--- vars/awscre.groovy
+++ vars/awscre.groovy
@@ -9,8 +9,7 @@ def call() {
         
         // Log in to AWS ECR using the injected credentials
         sh """
-            echo "Logging into AWS ECR..."
-            aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 975049929854.dkr.ecr.ap-southeast-1.amazonaws.com
+        aws sts get-caller-identity
         """
     }
 }
